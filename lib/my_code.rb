25665c7fa851b527ec88b93e49ba9f75e803842e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-24 03:45:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,4 @@
 (map_to_nagativize([1, ,2, 3, -9])) .to eq([-1, -2, -3, 9])
 (map_to_change(dune)) .to eq (dune)
 (map_to_double([1, 2, 3, -9]))) .to eq([2, 4, 6, -18])
+(map_to_square([1, 2, 3, -9]))) .to eq([1, 4, 9, 81])
